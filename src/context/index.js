ee291d2ec5d62ccd3ee089291633a35f3bc29b58
Darshan NDK <darshanndklp619@gmail.com>
2021-01-05 17:46:57 +0530

    update

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function AppProviders({ children }) {
-  return <Router>{children}</Router>;
+  return (
+    <ChakraProvider>
+      <Router>{children}</Router>
+    </ChakraProvider>
+  );
 }
 
 export { AppProviders };
